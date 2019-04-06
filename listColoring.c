7ba9d6d07254e1b158c70dd3353d0ca82014206e
aom035 <aom035@student.uib.no>
2019-04-06 15:25:56 +0200

    vertical constraints done

--- listColoring.c
+++ listColoring.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "listColoring.h"
 
 LCInstance *pigeonholeInstance(int rows, int columns);
 
-int main(){
+int s2n(char *str);
 
-    LCInstance *piggy = pigeonholeInstance(1, 2);
-    printf("Piggy Size: %lu\n", sizeof(piggy));
+int main() {
 
-    printf("SizeAlpha: %d\n", piggy->FinalColors[0]->sizeAlphabet);
+    LCInstance *piggy = pigeonholeInstance(2, 2);
+
+    printf("nRows: %d\n", piggy->nRows);
+    printf("nCols: %d\n", piggy->nColumns);
+
+    printf("%s\n", "IntermediateColors:");
+    for (int x = 0; x < piggy->nRows; ++x) {
+        for (int y = 0; y < piggy->nColumns; ++y) {
+            printf("\tAlphaSize[%d][%d]: %d\n", x, y, piggy->IntermediateColors[x][y].sizeAlphabet);
+            for (int i = 0; i < piggy->IntermediateColors[x][y].sizeAlphabet; ++i) {
+                printf("\t\t%d\t%s\n",
+                       piggy->IntermediateColors[x][y].S2N[i],
+                       piggy->IntermediateColors[x][y].N2S[i]);
+            }
+        }
+    }
+
+
+    int vConRow = piggy->nRows - 1;
+    int vConCol = piggy->nColumns + 1;
+
+    for (int x = 0; x < vConRow; ++x) {
+        for (int y = 0; y < vConCol; ++y) {
+            printf("nConstraints[%d][%d]: %d\n", x, y, piggy->vConstraints[x][y].nConstraints);
+            for (int i = 0; i < piggy->vConstraints[x][y].nConstraints; ++i) {
+                printf("(%d, %d) ", piggy->vConstraints[x][y].pairs[i].color1, piggy->vConstraints[x][y].pairs[i].color2);
+            }
+            printf("\n");
+            for (int i = 0; i < piggy->vConstraints[x][y].nConstraints; ++i) {
+                printf("(%s, %s) ", piggy->IntermediateColors[0][0].N2S[piggy->vConstraints[x][y].pairs[i].color1],
+                       piggy->IntermediateColors[0][0].N2S[piggy->vConstraints[x][y].pairs[i].color2]);
+            }
+            printf("\n");
+        }
+    }
+
     return 0;
 }
--- pigeonhole.c
+++ pigeonhole.c
@@ -3,8 +3,15 @@
 #include "listColoring.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 AlphabetMap *defaultColorMap(void);
+
+int s2n(char *str);
+
+ColorPair *defaultColorPairs();
+
+Constraint **buildVerticalMatrix(int rows, int columns);
 
 LCInstance *pigeonholeInstance(int rows, int columns) {
 
@@ -20,11 +27,44 @@ LCInstance *pigeonholeInstance(int rows, int columns) {
         pigeon->IntermediateColors[x] = malloc((unsigned long) columns * sizeof(AlphabetMap)); // FREE ME
         for (int y = 0; y < columns; ++y)
             pigeon->IntermediateColors[x][y] = *defaultMap;
+//            pigeon->IntermediateColors[x][y] = *defaultColorMap();
     }
 
+    // !ACHTUNG! FinalColors is now an ALIAS of IntermediateColors. TODO Ask Mateus.
     pigeon->FinalColors = pigeon->IntermediateColors;
 
+    pigeon->vConstraints = buildVerticalMatrix(rows, columns);
+
+//    pigeon->vConstraints
+
     return pigeon;
+}
+
+int s2n(char *str) {
+
+    if (!strcmp(str, "b"))
+        return 0;
+    if (!strcmp(str, "g"))
+        return 1;
+    if (!strcmp(str, "rr"))
+        return 2;
+    if (!strcmp(str, "bb"))
+        return 3;
+    if (!strcmp(str, "bg"))
+        return 4;
+    if (!strcmp(str, "br"))
+        return 5;
+    if (!strcmp(str, "gb"))
+        return 6;
+    if (!strcmp(str, "gg"))
+        return 7;
+    if (!strcmp(str, "gr"))
+        return 8;
+    else {
+        fprintf(stderr, "Bad input @ s2n().\n");
+        fprintf(stderr, "Input: %s\n", str);
+        exit(EXIT_FAILURE);
+    }
 }
 
 AlphabetMap *defaultColorMap(void) {
@@ -50,4 +90,46 @@ AlphabetMap *defaultColorMap(void) {
         defaultMap->S2N[i] = i; // TODO ASK MATEUS ABOUT THE ORDINALITY OF THE COLOR PERMUTATIONS.
 
     return defaultMap;
+}
+
+Constraint **buildVerticalMatrix(int rows, int columns) {
+
+    int i = rows - 1;
+    int j = columns + 1;
+    int numOfConstraints = 12;
+
+    ColorPair *defaultPairs = defaultColorPairs();
+
+    Constraint **vMatrix = malloc((unsigned long) i * sizeof(Constraint *)); // FREE ME
+    for (int x = 0; x < i; ++x) {
+        vMatrix[x] = malloc((unsigned long) j * sizeof(Constraint)); // FREE ME
+        for (int y = 0; y < j; ++y) {
+            vMatrix[x][y].nConstraints = numOfConstraints;
+            vMatrix[x][y].pairs = defaultPairs;
+        }
+    }
+    return vMatrix;
+}
+
+ColorPair *defaultColorPairs() {
+
+    int numOfConstraints = 12;
+    ColorPair *defaultPairs = malloc((unsigned long) numOfConstraints * sizeof(ColorPair)); // FREE ME
+    defaultPairs[0] = (ColorPair) {.color1 = s2n("bb"), .color2 = s2n("bb")};
+    defaultPairs[1] = (ColorPair) {.color1 = s2n("bb"), .color2 = s2n("gb")};
+    defaultPairs[2] = (ColorPair) {.color1 = s2n("gb"), .color2 = s2n("bb")};
+    defaultPairs[3] = (ColorPair) {.color1 = s2n("gb"), .color2 = s2n("gb")};
+
+    defaultPairs[4] = (ColorPair) {.color1 = s2n("bb"), .color2 = s2n("rr")};
+    defaultPairs[5] = (ColorPair) {.color1 = s2n("gb"), .color2 = s2n("rr")};
+
+    defaultPairs[6] = (ColorPair) {.color1 = s2n("rr"), .color2 = s2n("bg")};
+    defaultPairs[7] = (ColorPair) {.color1 = s2n("rr"), .color2 = s2n("gg")};
+
+    defaultPairs[8] = (ColorPair) {.color1 = s2n("bg"), .color2 = s2n("bg")};
+    defaultPairs[9] = (ColorPair) {.color1 = s2n("bg"), .color2 = s2n("gg")};
+    defaultPairs[10] = (ColorPair) {.color1 = s2n("gg"), .color2 = s2n("bg")};
+    defaultPairs[11] = (ColorPair) {.color1 = s2n("gg"), .color2 = s2n("gg")};
+
+    return defaultPairs;
 }
