c9ad22310e76cc5ee4118066c1c2bcba6e221f29
Markus Ragnhildstveit <markus.ragn@gmail.com>
2019-04-12 14:51:30 +0200

    refactoring and should be working now

--- nextLayer.c
+++ nextLayer.c
@@ -6,18 +6,21 @@
 #include "listColoring.h"
 
 NumSymbol memo(State l, Layer *layer);
-Layer* nextIncrement(LCInstance *instance, Layer *left, Layer *up, int i, int j);
+Layer *nextIncrement(LCInstance *instance, Layer *left, Layer *up, int i, int j);
 void copyStates(StateContainer *copyTo, StateContainer *copyFrom);
+void addTransitionsToLayer(LCInstance *instance, Layer *result, Layer *left, Layer *up, int i, int j);
+void addTransitionsByVAndHConstraints(Layer *result, Layer *left, Transition *upT, Constraint *vC, Constraint *hC, NumSymbol b);
 
-Layer* nextLayer(LCInstance *instance, int i, int j, Layer** m) 
+Layer *nextLayer(LCInstance *instance, int i, int j, Layer **m)
 {
-    if (j == 0) {
-        return nextIncrement(instance, NULL, &m[i-1][j], i, j);
+    if (j == 0)
+    {
+        return nextIncrement(instance, NULL, &m[i - 1][j], i, j);
     }
-    else {
-        return nextIncrement(instance, &m[i][j-1], &m[i-1][j], i, j);
+    else
+    {
+        return nextIncrement(instance, &m[i][j - 1], &m[i - 1][j], i, j);
     }
-    
 }
 
 //return symbol a iff every transition leading to state l at the right frontier of layer is equal to a,
@@ -39,11 +42,11 @@ NumSymbol memo(State l, Layer *layer)
     exit(-1);
 }
 
-Layer* nextIncrement(LCInstance *instance, Layer *left, Layer *up, int i, int j)
+Layer *nextIncrement(LCInstance *instance, Layer *left, Layer *up, int i, int j)
 {
     Layer *result = (Layer *)malloc(sizeof(Layer));
     if (left != NULL) // j != 0
-    { 
+    {
         copyStates(&result->leftStates, &left->rightStates);
         result->initialStates.nStates = 0;
         result->initialFlag = 0;
@@ -57,58 +60,80 @@ Layer* nextIncrement(LCInstance *instance, Layer *left, Layer *up, int i, int j)
         result->initialFlag = 1;
     }
     result->width = fmax(result->leftStates.nStates, result->rightStates.nStates);
-    
+
     copyStates(&result->rightStates, &up->rightStates);
     copyStates(&result->finalStates, &up->finalStates);
     result->finalFlag = up->finalFlag;
     result->map = instance->IntermediateColors[i][j];
-    result->transitions.nTransitions = 0;
 
+    addTransitionsToLayer(instance, result, left, up, i, j);
+    // result->transitions.nTransitions = 0;
+
+    //assume i != 0.
+
+    return result;
+}
+
+void addTransitionsToLayer(LCInstance *instance, Layer *result, Layer *left, Layer *up, int i, int j)
+{
+    result->transitions.nTransitions = 0;
     //assume i != 0.
 
     //Assume this is the size for now
     result->transitions.set = malloc(up->transitions.nTransitions * sizeof(Transition));
+    Constraint *vC = &instance->vConstraints[i - 1][j];
+    Constraint *hC = (j == 0 ? NULL : &instance->hConstraints[i][j - 1]);
 
-    for (int transIndex = 0; transIndex < up->transitions.nTransitions; transIndex++) {
-
-        NumSymbol a = up->transitions.set[transIndex].a;
-
-        for (int colorIndex = 0; colorIndex < instance->IntermediateColors[i][j].sizeAlphabet; colorIndex++) {
-
+    // Check for all transitions in up
+    for (int transIndex = 0; transIndex < up->transitions.nTransitions; transIndex++)
+    {
+        for (int colorIndex = 0; colorIndex < instance->IntermediateColors[i][j].sizeAlphabet; colorIndex++)
+        {
             NumSymbol b = instance->IntermediateColors[i][j].S2N[colorIndex];
+            addTransitionsByVAndHConstraints(result, left, &up->transitions.set[transIndex], vC, hC, b);
+        }
+    }
+}
 
-            for (int vertical = 0; vertical < instance->vConstraints[i-1][j].nConstraints; vertical++) {
-                if (instance->vConstraints[i-1][j].pairs[vertical].color1 == a && instance->vConstraints[i-1][j].pairs[vertical].color2 == b) {
-                    
-                    if (j == 0) {
-                        result->transitions.set[result->transitions.nTransitions].s1 = up->transitions.set[transIndex].s1;
+void addTransitionsByVAndHConstraints(Layer *result, Layer *left, Transition *upT, Constraint *vC, Constraint *hC, NumSymbol b)
+{
+    NumSymbol a = upT->a;
+
+    for (int vertical = 0; vertical < vC->nConstraints; vertical++)
+    {
+        if (vC->pairs[vertical].color1 == a && vC->pairs[vertical].color2 == b)
+        {
+
+            if (hC == NULL)
+            {
+                result->transitions.set[result->transitions.nTransitions].s1 = upT->s1;
+                result->transitions.set[result->transitions.nTransitions].a = b;
+                result->transitions.set[result->transitions.nTransitions].s2 = upT->s2;
+                result->transitions.nTransitions++;
+            }
+            else
+            {
+                NumSymbol m = memo(upT->s1, left);
+                for (int horisontal = 0; horisontal < hC->nConstraints; horisontal++)
+                {
+                    if (hC->pairs[horisontal].color1 == m && hC->pairs[horisontal].color2 == b)
+                    {
+                        result->transitions.set[result->transitions.nTransitions].s1 = upT->s1;
                         result->transitions.set[result->transitions.nTransitions].a = b;
-                        result->transitions.set[result->transitions.nTransitions].s2 = up->transitions.set[transIndex].s2;
+                        result->transitions.set[result->transitions.nTransitions].s2 = upT->s2;
                         result->transitions.nTransitions++;
-                    } else {
-                        NumSymbol m = memo(up->transitions.set[transIndex].s1, left);
-                        for (int horisontal = 0; horisontal < instance->hConstraints[i][j-1].nConstraints; horisontal++) {
-                            if (instance->hConstraints[i][j-1].pairs[horisontal].color1 == m && instance->hConstraints[i][j-1].pairs[horisontal].color2 == b) {
-                                result->transitions.set[result->transitions.nTransitions].s1 = up->transitions.set[transIndex].s1;
-                                result->transitions.set[result->transitions.nTransitions].a = b;
-                                result->transitions.set[result->transitions.nTransitions].s2 = up->transitions.set[transIndex].s2;
-                                result->transitions.nTransitions++;
-                            }
-                        }
-                    }   
-                }   
+                    }
+                }
             }
         }
     }
-
-    return result;
 }
 
 void copyStates(StateContainer *copyTo, StateContainer *copyFrom)
 {
     copyTo->nStates = copyFrom->nStates;
     copyTo->set = malloc(copyTo->nStates * sizeof(State));
-    for(int i = 0; i < copyFrom->nStates; i++)
+    for (int i = 0; i < copyFrom->nStates; i++)
     {
         copyTo->set[i] = copyFrom->set[i];
     }
