aef6e1c670ef1ba858fa5ea615f796da0953cca7
Markus Ragnhildstveit <markus.ragn@gmail.com>
2019-04-12 14:56:47 +0200

    revert master

--- listColoring.c
+++ listColoring.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "listColoring.h"
-// #include "isThereSolution.c"
 
 int main(){
     // printf() displays the string inside quotation
-    // nextLayer()
-    LCInstance inst;
-    readLCInstance("listColoringExample.tex", &inst);
-    
-    ODD firstRow; 
-    rowToODD(&inst, 0, &firstRow);
-    
-    Layer *t1 = nextIncrement(&inst, NULL, &firstRow.layerSequence[0], 1, 0);
-    Layer *t2 = nextIncrement(&inst, t1, &firstRow.layerSequence[0], 1, 1);
-    
-
-    printLCInstance("listColoringExample.txt", &inst);
-
-    printf("hello\n");
+    printf("Hello, World!");
     return 0;
 }
