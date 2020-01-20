870ef3d6d2c707803c0da14c2abdbb96cc6e0b3c
Vasily Alferov <vasily.v.alferov@gmail.com>
2020-01-20 11:11:27 +0100

    Delete the prototype

--- listColoring.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include "listColoring.h"
-
-int main(){
-    // printf() displays the string inside quotation
-    printf("Hello, World!");
-    return 0;
-}
